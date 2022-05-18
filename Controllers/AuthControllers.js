b3a3404076caa14d365bd95d4bda822cea051ff2
kaeriv93 <vsan93@yahoo.com>
2022-05-18 14:10:43 -0500

    Push to backend

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -8,6 +8,27 @@ const createToken = (id) =>{
     })
 }
 
+const handleErrors = (err) =>{
+    let errors = { email:"", password:""}
+
+    if(err.message==="incorrect Email") 
+    errors.email = "That email or password is not found"
+
+    if(err.message==="incorrect password") 
+    errors.password = "That email or password is not found"
+
+    if(err.code ===11000){
+        errors.email="Email is already in use"
+        return errors
+    }
+    if(err.message.includes("Users validation failed")){
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+}
+
 module.exports.register = async (req,res,next) =>{ try{
     const {email,password,firstName,lastName,username} = req.body
     const user = await UserModel.create({email,password,firstName,lastName,username})
@@ -20,8 +41,23 @@ module.exports.register = async (req,res,next) =>{ try{
     res.status(201).json({user:user._id, created:true})
 }catch(err){
     console.log(err)
-    req.error= error
+    const errors = handleErrors(err)
     res.json({errors, created:false})
 }}
 
-module.exports.login = async (req,res,next) =>{}
+module.exports.login = async (req,res) =>{
+    const {email,password} = req.body
+    try{
+    const user = await UserModel.login(email,password)
+    const token = createToken(user._id)
+    res.cookie('jwt', token,{
+        withCredentials:true,
+        httpOnly:false,
+        maxAge: maxAge * 1000,
+    })
+    res.status(200).json({user:user._id, created:true})
+}catch(err){
+    console.log(err)
+    const errors = handleErrors(err)
+    res.json({errors, created:false})
+}}
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcyrpt = require ('bcrypt')
+const bcrypt = require('bcryptjs/dist/bcrypt')
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -41,8 +42,22 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.pre('save', async function(next){
-
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
+
+userSchema.statics.login = async function(email,password){
+    const user = await this.findOne({email})
+    if(user){
+        const auth = await bcrypt.compare(password,user.password)
+        if(auth){
+            return user
+        }
+        throw Error("incorrect password")
+    }
+    throw Error('incorrect Email')
+}
 
 const User = mongoose.model('User', userSchema)
 
